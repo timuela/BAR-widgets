6932d241f911c1c114643cfe19b483aa6cc2f432
Timuela <kevingom43@gmail.com>
2025-09-04 00:03:17 +0700

    remove color

--- queen_ping.lua
+++ queen_ping.lua
@@ -11,30 +11,11 @@ function widget:GetInfo()
     }
 end
 
--- Helper function to convert RGB values to Spring-compatible string format
-local function colourNames(R, G, B)
-    local R255 = math.floor(R * 255)
-    local G255 = math.floor(G * 255)
-    local B255 = math.floor(B * 255)
-    if R255 % 10 == 0 then
-        R255 = R255 + 1
-    end
-    if G255 % 10 == 0 then
-        G255 = G255 + 1
-    end
-    if B255 % 10 == 0 then
-        B255 = B255 + 1
-    end
-    return "\255" .. string.char(R255) .. string.char(G255) .. string.char(B255)
-end
-
--- Define target unitDefIDs based on names
 local TARGET_UNITDEF_NAMES = {
     "raptor_queen_easy", "raptor_queen_veryeasy",
     "raptor_queen_hard", "raptor_queen_veryhard",
     "raptor_queen_epic",
     "raptor_queen_normal",
-    -- "raptor_miniq_a", "raptor_miniq_b", "raptor_miniq_c",
 }
 
 local raptorQueenDefIDs = {}
@@ -52,9 +33,8 @@ function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
     if raptorQueenDefIDs[unitDefID] then
         local x, y, z = Spring.GetUnitPosition(unitID)
         if x and y and z then
-            local color = {1, 0, 0, 1} -- Red color for the ping
             local pingText = "Raptor Queen Killed!"
-            Spring.MarkerAddPoint(x, y, z, colourNames(color[1], color[2], color[3]) .. pingText, false)
+            Spring.MarkerAddPoint(x, y, z, pingText, false)
         end
     end
 end
